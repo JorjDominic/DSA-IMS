d8c32b27678f9e4c4fae50173afbc05193dec826
Jorj <166830432+JorjDominic@users.noreply.github.com>
2024-10-29 23:10:34 +0800

    Delete DSA PROJECT directory

--- DSA PROJECT/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Cache-Control: no-cache, must-revalidate, no-store, max-age=0, post-check=0, pre-check=0");
-header("Pragma: no-cache");
-header("Expires: 0");
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("location: login.php");
-    exit;
-}
